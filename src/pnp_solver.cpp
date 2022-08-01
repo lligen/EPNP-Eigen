0ab9557dffb5768e385da22fc2c7c4df3e5dfa42
lligen <lligen.dai@gmail.com>
2022-08-01 17:10:43 +0800

    Update pnp_solver.cpp

--- src/pnp_solver.cpp
+++ src/pnp_solver.cpp
@@ -52,10 +52,9 @@ void PnpSolver::choose_control_points()
     
   PW0tPW0=PW0.transpose()*PW0;
   
-  //TODO:maybe wrong
-  Eigen::JacobiSVD<Eigen::Matrix3d> svd(PW0tPW0,Eigen::ComputeFullU|Eigen::ComputeFullV);
+  Eigen::JacobiSVD<Eigen::Matrix3d> svd(PW0tPW0,Eigen::ComputeFullU);
   DC=svd.singularValues();
-  UCt=svd.matrixV().transpose();
+  UCt=svd.matrixU().transpose();
   
   for(int i=1;i<4;i++)
   {
@@ -138,10 +137,10 @@ void PnpSolver::find_betas_0(const Eigen::Matrix<double,6,10> &L_6x10,const Eige
   Eigen::Matrix<double,6,4> L_6x4;
   Eigen::Vector4d B4;
   
-  L_6x4.col(0)=L_6x10.col(0);
-  L_6x4.col(1)=L_6x10.col(1);
-  L_6x4.col(2)=L_6x10.col(3);
-  L_6x4.col(3)=L_6x10.col(6);
+  L_6x4.col(0) = L_6x10.block<6, 1>(0, 0);
+  L_6x4.col(1) = L_6x10.block<6, 1>(0, 1);
+  L_6x4.col(2) = L_6x10.block<6, 1>(0, 3);
+  L_6x4.col(3) = L_6x10.block<6, 1>(0, 6);
   
   B4=L_6x4.colPivHouseholderQr().solve(Rho);
   
@@ -166,9 +165,9 @@ void PnpSolver::find_betas_1(const Eigen::Matrix<double,6,10> &L_6x10,const Eige
   Eigen::Matrix<double,6,3> L_6x3;
   Eigen::Vector3d B3;
   
-  L_6x3.col(0)=L_6x10.col(0);
-  L_6x3.col(1)=L_6x10.col(1);
-  L_6x3.col(2)=L_6x10.col(2);
+  L_6x3.col(0) = L_6x10.block<6, 1>(0, 0);
+  L_6x3.col(1) = L_6x10.block<6, 1>(0, 1);
+  L_6x3.col(2) = L_6x10.block<6, 1>(0, 2);
   
   B3=L_6x3.colPivHouseholderQr().solve(Rho);
   
@@ -194,11 +193,11 @@ void PnpSolver::find_betas_2(const Eigen::Matrix<double,6,10> &L_6x10,const Eige
   Eigen::Matrix<double,6,5> L_6x5;
   Eigen::Matrix<double,5,1> B5;
   
-  L_6x5.col(0)=L_6x10.col(0);
-  L_6x5.col(1)=L_6x10.col(1);
-  L_6x5.col(2)=L_6x10.col(2);
-  L_6x5.col(3)=L_6x10.col(3);
-  L_6x5.col(4)=L_6x10.col(4);
+  L_6x5.col(0) = L_6x10.block<6, 1>(0, 0);
+  L_6x5.col(1) = L_6x10.block<6, 1>(0, 1);
+  L_6x5.col(2) = L_6x10.block<6, 1>(0, 2);
+  L_6x5.col(3) = L_6x10.block<6, 1>(0, 3);
+  L_6x5.col(4) = L_6x10.block<6, 1>(0, 4);
   
   B5=L_6x5.colPivHouseholderQr().solve(Rho);
   
@@ -224,7 +223,7 @@ void PnpSolver::compute_ccs(const Eigen::Vector4d &Betas,const Eigen::Matrix<dou
   for(int i=0;i<4;i++)
   {
     for(int j=0;j<4;j++)
-      for(int k=0;k<4;k++)
+      for(int k=0;k<3;k++)
 	ccs(j,k)+=Betas[i]*Ut(11-i,3*j+k);
   }
 }
@@ -268,6 +267,13 @@ void PnpSolver::estimate_R_and_t(Eigen::Matrix3d &R,Eigen::Vector3d &T)
   Eigen::Matrix3d V=svd.matrixV();
   
   R=U*(V.transpose());
+  const double det = R.determinant();
+  if (det < 0)
+  {
+      R(2, 0) = -R(2, 0);
+      R(2, 1) = -R(2, 1);
+      R(2, 2) = -R(2, 2);
+  }
   T=pc0-R*pw0;
 }
 double PnpSolver::reprojection_error(const Eigen::Matrix3d &R,const Eigen::Vector3d &T)
@@ -281,7 +287,7 @@ double PnpSolver::reprojection_error(const Eigen::Matrix3d &R,const Eigen::Vecto
     point2d[0]=cx+fx*point3d[0]/point3d[2];
     point2d[1]=cy+fy*point3d[1]/point3d[2];
     
-    sum2+=(us.row(i).transpose()-point2d).squaredNorm();
+    sum2+=(us.row(i).transpose()-point2d).norm();
   }
   return sum2/number_of_correspondences;
 }
@@ -324,15 +330,13 @@ double PnpSolver::compute_pose(Eigen::Matrix3d &R,Eigen::Vector3d &T)
   Eigen::Matrix<double,12,12> Ut;
   
   MtM=M.transpose()*M;
-  //TODO:maybe wrong
-  Eigen::JacobiSVD<Eigen::Matrix<double,12,12> > svd(MtM,Eigen::ComputeThinU|Eigen::ComputeThinV);
+  Eigen::JacobiSVD<Eigen::Matrix<double,12,12> > svd(MtM,ComputeFullU);
   D=svd.singularValues().transpose();
   Ut=svd.matrixU().transpose();
   
   Eigen::Matrix<double,6,10> L_6x10;
   Eigen::Matrix<double,6,1> Rho;
   
-  //TODO
   compute_L_6x10(Ut,L_6x10);
   compute_rho(Rho);
   
@@ -342,12 +346,15 @@ double PnpSolver::compute_pose(Eigen::Matrix3d &R,Eigen::Vector3d &T)
   double errors[3];
   
   find_betas_0(L_6x10,Rho,Betas0);
+  gauss_newton(L_6x10, Rho, Betas0);
   errors[0]=compute_R_and_t(Ut,Betas0,R0,T0);
   
   find_betas_1(L_6x10,Rho,Betas1);
+  gauss_newton(L_6x10, Rho, Betas1);
   errors[1]=compute_R_and_t(Ut,Betas1,R1,T1);
   
   find_betas_2(L_6x10,Rho,Betas2);
+  gauss_newton(L_6x10, Rho, Betas2);
   errors[2]=compute_R_and_t(Ut,Betas2,R2,T2);
   
   int N=0;
@@ -362,5 +369,74 @@ double PnpSolver::compute_pose(Eigen::Matrix3d &R,Eigen::Vector3d &T)
   }
   return errors[N]; 
 }
+void PnpSolver::project_oints(std::vector<Eigen::Vector3d>& src,
+                              std::vector<Eigen::Vector2d>& dst,
+                              Eigen::Matrix3d&              R,
+                              Eigen::Vector3d&              T)
+{
+    int size = src.size();
+    for (int i = 0; i < size; ++i)
+    {
+        Eigen::Vector3d point3d = R * src[i] + T;
 
+        dst[i][0] = cx + fx * point3d[0] / point3d[2];
+        dst[i][1] = cy + fy * point3d[1] / point3d[2];
+    }
+}
+
+void PnpSolver::gauss_newton(const Eigen::Matrix<double, 6, 10>& L_6x10,
+                             const Eigen::Matrix<double, 6, 1>&  Rho,
+                             Eigen::Vector4d&                    betas)
+{
+    const int iterations_number = 5;
+
+    Eigen::Matrix<double, 6, 4> A;
+    Eigen::Matrix<double, 6, 1> B;
+    Eigen::Vector4d             X;
+
+    for (int k = 0; k < iterations_number; k++)
+    {
+        compute_A_and_b_gauss_newton(L_6x10, Rho, betas, A, B);
+        qr_solve(A, B, X);
+
+        for (int i = 0; i < 4; i++)
+            betas[i] += X[i];
+    }
+}
+
+void PnpSolver::compute_A_and_b_gauss_newton(const Eigen::Matrix<double, 6, 10>& L_6x10,
+                                             const Eigen::Matrix<double, 6, 1>&  Rho,
+                                             Eigen::Vector4d&                    betas,
+                                             Eigen::Matrix<double, 6, 4>&        A,
+                                             Eigen::Matrix<double, 6, 1>&        b)
+{
+    for (int i = 0; i < 6; ++i)
+    {
+        Eigen::RowVectorXd rowL = L_6x10.row(i);
+        Eigen::RowVector4d rowA = A.row(i);
+
+        rowA[0] = 2 * rowL[0] * betas[0] + rowL[1] * betas[1] + rowL[3] * betas[2] +
+                  rowL[6] * betas[3];
+        rowA[1] = rowL[1] * betas[0] + 2 * rowL[2] * betas[1] + rowL[4] * betas[2] +
+                  rowL[7] * betas[3];
+        rowA[2] = rowL[3] * betas[0] + rowL[4] * betas[1] + 2 * rowL[5] * betas[2] +
+                  rowL[8] * betas[3];
+        rowA[3] = rowL[6] * betas[0] + rowL[7] * betas[1] + rowL[8] * betas[2] +
+                  2 * rowL[9] * betas[3];
+
+        b[i]     = Rho[i] - (rowL[0] * betas[0] * betas[0] + rowL[1] * betas[0] * betas[1] +
+                         rowL[2] * betas[1] * betas[1] + rowL[3] * betas[0] * betas[2] +
+                         rowL[4] * betas[1] * betas[2] + rowL[5] * betas[2] * betas[2] +
+                         rowL[6] * betas[0] * betas[3] + rowL[7] * betas[1] * betas[3] +
+                         rowL[8] * betas[2] * betas[3] + rowL[9] * betas[3] * betas[3]);
+        A.row(i) = rowA;
+    }
+}
+
+void PnpSolver::qr_solve(Eigen::Matrix<double, 6, 4>& A,
+                         Eigen::Matrix<double, 6, 1>& B,
+                         Eigen::Vector4d&             X)
+{
+    X = A.colPivHouseholderQr().solve(B);
+}
 
